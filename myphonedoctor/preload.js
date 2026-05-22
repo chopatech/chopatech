aaa7971fd6c14d427a9aa9225d24f2a4552a9ff2
chopatech <chopatechtz@gmail.com>
2026-05-22 18:32:35 +0300

    change

--- myphonedoctor/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('androidAPI', {
-  adbDevices: () => ipcRenderer.invoke('adb-devices'),
-  fastbootDevices: () => ipcRenderer.invoke('fastboot-devices'),
-  flashBoot: (imagePath) => ipcRenderer.invoke('flash-boot', imagePath)
-});
