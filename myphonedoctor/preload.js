d9e7409454ae57a2f6ca16b9ad921062a4576b4f
chopatech <chopatechtz@gmail.com>
2026-05-29 11:03:53 +0300

    change

--- myphonedoctor/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('androidAPI', {
-  onDeviceStatus: (callback) => {
-    ipcRenderer.on('device-status', (event, data) => {
-      callback(data);
-    });
-  }
-});
